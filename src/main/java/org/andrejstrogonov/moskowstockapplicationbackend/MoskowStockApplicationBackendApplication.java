19deb57c9fbcc5ac5a761f8f0bded50789b96fb9
andrejstrogonov <strogonovandrej@hotmail.com>
2026-04-16 11:09:26 +0300

    Initial commit

---
+++ src/main/java/org/andrejstrogonov/moskowstockapplicationbackend/MoskowStockApplicationBackendApplication.java
@@ -0,0 +1,13 @@
+package org.andrejstrogonov.moskowstockapplicationbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoskowStockApplicationBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoskowStockApplicationBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/andrejstrogonov/moskowstockapplicationbackend/MoskowStockApplicationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.andrejstrogonov.moskowstockapplicationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoskowStockApplicationBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
